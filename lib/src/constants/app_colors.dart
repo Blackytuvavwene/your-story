cdb9eb9b7bd25e435c16c08e5843cd25dd105d71
Boitumelo Tubabwene <btexblacky@gmail.com>
2022-01-13 19:09:33 +0200

    second commit

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/painting.dart';
+
+// custom app colors
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF3F784C);
+  static const Color darkGreen = Color(0xFF313715);
+  static const Color lightGreen = Color(0xFF96E072);
+  static const Color yellowGreen = Color(0xFFE8FCCF);
+  static const Color roseEbony = Color(0xFF664147);
+  static const Color blackCoffee = Color(0xFF483D3F);
+}
